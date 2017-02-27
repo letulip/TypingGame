a32c02eb3bd88fac5f63dd8ecd70958e8f751dfd
letulip <ivladimirskiy@ya.ru>
2017-02-27 19:18:21 +0300

    Timer set
    Timer checks ListBox if it filled - Game's over

--- TypingGame/Form1.Designer.cs
+++ TypingGame/Form1.Designer.cs
@@ -29,11 +29,116 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.progBarDifficulty = new System.Windows.Forms.ToolStripProgressBar();
+            this.lblCorrect = new System.Windows.Forms.ToolStripStatusLabel();
+            this.lblMissed = new System.Windows.Forms.ToolStripStatusLabel();
+            this.lblTotal = new System.Windows.Forms.ToolStripStatusLabel();
+            this.lblAccuracy = new System.Windows.Forms.ToolStripStatusLabel();
+            this.lblDifficulty = new System.Windows.Forms.ToolStripStatusLabel();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 72F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 108;
+            this.listBox1.Location = new System.Drawing.Point(0, 0);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(860, 136);
+            this.listBox1.TabIndex = 0;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 800;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.lblCorrect,
+            this.lblMissed,
+            this.lblTotal,
+            this.lblAccuracy,
+            this.lblDifficulty,
+            this.progBarDifficulty});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 114);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(860, 22);
+            this.statusStrip1.SizingGrip = false;
+            this.statusStrip1.TabIndex = 1;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // progBarDifficulty
+            // 
+            this.progBarDifficulty.Name = "progBarDifficulty";
+            this.progBarDifficulty.Size = new System.Drawing.Size(100, 16);
+            // 
+            // lblCorrect
+            // 
+            this.lblCorrect.Name = "lblCorrect";
+            this.lblCorrect.Size = new System.Drawing.Size(58, 17);
+            this.lblCorrect.Text = "Correct: 0";
+            // 
+            // lblMissed
+            // 
+            this.lblMissed.Name = "lblMissed";
+            this.lblMissed.Size = new System.Drawing.Size(56, 17);
+            this.lblMissed.Text = "Missed: 0";
+            // 
+            // lblTotal
+            // 
+            this.lblTotal.Name = "lblTotal";
+            this.lblTotal.Size = new System.Drawing.Size(46, 17);
+            this.lblTotal.Text = "Total: 0";
+            // 
+            // lblAccuracy
+            // 
+            this.lblAccuracy.Name = "lblAccuracy";
+            this.lblAccuracy.Size = new System.Drawing.Size(78, 17);
+            this.lblAccuracy.Text = "Accuracy: 0%";
+            // 
+            // lblDifficulty
+            // 
+            this.lblDifficulty.Name = "lblDifficulty";
+            this.lblDifficulty.Size = new System.Drawing.Size(505, 17);
+            this.lblDifficulty.Spring = true;
+            this.lblDifficulty.Text = "Difficulty";
+            this.lblDifficulty.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(860, 136);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.listBox1);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Hit the Keys!";
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel lblCorrect;
+        private System.Windows.Forms.ToolStripStatusLabel lblMissed;
+        private System.Windows.Forms.ToolStripStatusLabel lblTotal;
+        private System.Windows.Forms.ToolStripStatusLabel lblAccuracy;
+        private System.Windows.Forms.ToolStripStatusLabel lblDifficulty;
+        private System.Windows.Forms.ToolStripProgressBar progBarDifficulty;
     }
 }
 
--- TypingGame/Form1.cs
+++ TypingGame/Form1.cs
@@ -16,5 +16,21 @@ namespace TypingGame
         {
             InitializeComponent();
         }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            Random rnd = new Random();
+
+            //add a random key to the ListBox
+            listBox1.Items.Add((Keys)rnd.Next(65, 90));
+
+            //check ListBox if it filled 
+            if(listBox1.Items.Count > 7)
+            {
+                listBox1.Items.Clear();
+                listBox1.Items.Add("Game over!");
+                timer1.Stop();
+            }
+        }
     }
 }
